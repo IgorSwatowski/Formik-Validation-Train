4a651be9770fd08a0189565a721373fc53e0cb9d
Igor Swatowski <igi66@wp.pl>
2021-01-27 11:54:11 +0100

    Prepared files structure. Deleted unused imports like tests.

---
+++ src/views/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Views
+import Form from '../views/Formik/Form';
+
+function Root() {
+  return (
+    <div className="App">
+        <Form />
+    </div>
+  );
+}
+
+export default Root;
